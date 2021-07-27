721501242734a96a2068c8586b2ab81c2423df05
Joaquin Viretti <joaquin.viretti@gmail.com>
2021-07-27 20:04:13 -0300

    agrego entidades

---
+++ src/main/java/egg/tienda/productos/entidades/Producto.java
@@ -0,0 +1,99 @@
+package egg.tienda.productos.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Producto {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String nombre;
+    private Integer precio;
+    private String descripcion;
+    
+    @OneToOne
+    private Fabricante fabricante;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fabricante
+     */
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    /**
+     * @param fabricante the fabricante to set
+     */
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
